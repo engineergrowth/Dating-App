75db0b88f7c25e6a76dd1846e0fb32bdcebf7b84
admin <admin@air-khxng-admin.mynetworksettings.com>
2024-01-12 11:00:13 -0500

    adding landing page css

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,8 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LandingPage from "./components/LandingPage";
+import  './App.css';
+import React, { useState } from "react";
+
 const App = () => {
+  const [currentForm, setCurrentForm] = useState('LandingPage');
   return (
+
+
     <Router>
         <Nav />
         <Routes>
